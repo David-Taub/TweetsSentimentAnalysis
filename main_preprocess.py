478f6bb2ececf665d3460949e071ae331aede55a
David Ariel <davidariel333@gmail.com>
2018-09-30 00:21:44 +0300

    split into functions added GAP for later knowledge of words effect on outcome gets acc .98 F1 .78 recall .67  and precision .90

--- main_preprocess.py
+++ main_preprocess.py
@@ -1,22 +1,27 @@
+import keras
+import sklearn.utils.class_weight
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
-from sklearn.model_selection import train_test_split
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D, GlobalAveragePooling1D, Permute, Activation, Conv1D
+from keras.callbacks import Callback
 from keras.utils.np_utils import to_categorical
+from sklearn.metrics import f1_score, recall_score, precision_score
 import re
 
 TRAIN_CSV_FILEPATH = r'C:\Users\booga\Dropbox\projects\TweetsSentimentAnalysis\data\train.csv'
 TEST_CSV_FILEPATH = r'C:\Users\booga\Dropbox\projects\TweetsSentimentAnalysis\data\test.csv'
-
+MODEL_FILEPATH = r'C:\Users\booga\Dropbox\projects\TweetsSentimentAnalysis\model.h5'
+VOCAB_SIZE = 30000
 
 def hashtag_split_func(x):
     return " ".join([a for a in re.split('([A-Z][a-z]+)', x.group(1)) if a])
 
 
+# string manipulation that removes links, breaks hashtags lower case etc
 def preprocess_data(data_raw):
     data = data_raw
     # split hashtags
@@ -37,35 +42,77 @@ def preprocess_data(data_raw):
     data = data[np.vectorize(lambda x: len(x) > 0)(data[:, 1]), :]
     return data
 
-
-train_val_data_raw = pd.read_csv(TRAIN_CSV_FILEPATH).values
-train_val_data = preprocess_data(train_val_data_raw)
-
-print(train_val_data[train_val_data[:, 2] == 0].size)
-print(train_val_data[train_val_data[:, 2] == 1].size)
-print(train_val_data[train_val_data[:, 2] == 0].size / train_val_data.size)
-
-VOCAB_SIZE = 30000
-tokenizer = Tokenizer(num_words=VOCAB_SIZE, split=' ')
-tokenizer.fit_on_texts(train_val_data[:, 1])
-X = tokenizer.texts_to_sequences(train_val_data[:, 1])
-X = pad_sequences(X)
-Y = to_categorical(train_val_data[:, 2])
+#used to print F1 recall and precision while training, not used for early stop
+class Metrics(Callback):
+    def on_epoch_end(self, batch, logs={}):
+        predict = np.argmax(np.asarray(self.model.predict(self.validation_data[0])), axis=1)
+        targ = np.argmax(self.validation_data[1], axis=1)
+        print('val_f1 %.3f' % f1_score(targ, predict))
+        print('val_recall %.3f' % recall_score(targ, predict))
+        print('val_precision %.3f' % precision_score(targ, predict))
+        return
 
 
 
-EMBEDDED_DIM = 500
-LSTM_OUT = 196
-model = Sequential()
-model.add(Embedding(VOCAB_SIZE, EMBEDDED_DIM, input_length=X.shape[1]))
-model.add(SpatialDropout1D(0.4))
-model.add(LSTM(LSTM_OUT, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, validation_split=0.15, epochs=50, batch_size=32)
+def predict_test(tokenizer, max_words_in_tweets):
+    test_data_raw = pd.read_csv(TEST_CSV_FILEPATH).values
+    test_data = preprocess_data(test_data_raw)
+    X_test = tokenizer.texts_to_sequences(test_data[:, 1])
+    X_test = pad_sequences(X_test, max_words_in_tweets)
+    model = keras.models.load_model(MODEL_FILEPATH)
+    model.predict(X_test)
 
 
-# test_data_raw = pd.read_csv(TEST_CSV_FILEPATH).values
-# test_data = preprocess_data(test_data_raw)
-# X_test = tokenizer.texts_to_sequences(test_data[:, 1])
-# X_test = pad_sequences(X_test, train_val_data.shape[1])
+def build_model(input_length):
+    #TODO: use glove embedding
+    #TODO: pretrain the net as a LM
+    model = Sequential()
+    model.add(Embedding(VOCAB_SIZE, 300, input_length=input_length))
+    model.add(SpatialDropout1D(0.5))
+    # model.add(LSTM(100, dropout=0.3, recurrent_dropout=0.3))
+    # model.add(Dense(2, activation='softmax'))
+
+    model.add(LSTM(200, return_sequences=True, dropout=0.5, recurrent_dropout=0.5))
+    model.add(Conv1D(filters=2, kernel_size=1, activation='relu'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Activation(activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    return model
+
+def main():
+    train_val_data_raw = pd.read_csv(TRAIN_CSV_FILEPATH).values
+    train_val_data = preprocess_data(train_val_data_raw)
+
+    print('Negatives: %d' % train_val_data[train_val_data[:, 2] == 0].size)
+    print('Positives: %d' % train_val_data[train_val_data[:, 2] == 1].size)
+    print('Baseline accuracy: %.3f' % (train_val_data[train_val_data[:, 2] == 0].size / train_val_data.size))
+
+    tokenizer = Tokenizer(num_words=VOCAB_SIZE, split=' ')
+    tokenizer.fit_on_texts(train_val_data[:, 1])
+
+    X = tokenizer.texts_to_sequences(train_val_data[:, 1])
+    X = pad_sequences(X)
+    Y = to_categorical(train_val_data[:, 2])
+    sequence_words_amount = X.shape[1]
+
+    model = build_model(sequence_words_amount)
+    # class_weight used to balance type I and type II errors due to imbalanced dataset
+    class_weight = sklearn.utils.class_weight.compute_class_weight('balanced',
+                                                                   np.unique(train_val_data[:, 2]),
+                                                                   train_val_data[:, 2])
+    callbacks = [EarlyStopping(patience=3),
+                 ModelCheckpoint(MODEL_FILEPATH, save_best_only=True),
+                 Metrics()]
+    model.fit(X, Y,
+              validation_split=0.25,
+              epochs=50,
+              batch_size=64,
+              callbacks=callbacks,
+              class_weight=class_weight)
+
+    predict_test(tokenizer, sequence_words_amount)
+
+
+if __name__ == '__main__':
+    main()
